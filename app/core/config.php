58cd25a8ddd3ed513a1c251087ccb68e2aea5194
maskimov <146643807+maskimov@users.noreply.github.com>
2024-06-18 00:30:27 +0300

    Update config.php

--- app/core/config.php
+++ app/core/config.php
@@ -5,9 +5,9 @@ defined('ROOTPATH') OR exit('Access Denied!');
 if($_SERVER['SERVER_NAME'] == 'localhost')
 {
 	/** database config **/
-	define('DBNAME', 'furniture');
-	define('DBHOST', 'localhost');
-	define('DBUSER', 'root');
+	define('DBNAME', 'XXXXX');
+	define('DBHOST', 'XXXXX');
+	define('DBUSER', 'XXXXX');
 	define('DBPASS', '');
 	define('DBDRIVER', '');
 	
@@ -26,14 +26,14 @@ if($_SERVER['SERVER_NAME'] == 'localhost')
 
 }
 
-define('MAIL_HOST','smtp.gmail.com');
-define('MAIL_USERNAME','kozyriev.developer@gmail.com');
-define('MAIL_PASSWORD','vtly ibtt ulog wigk');
-define('MAIL_SEND_FROM','kozyriev.developer@gmail.com');
-define('MAIL_SEND_FROM_NAME','Furniture Factory');
-define('MAIL_REPLY_TO','kozyriev.developer@gmail.com');
-define('MAIL_REPLY_TO_NAME','Maksim Kozyriev');
-define('MAIL_PORT',587);
+define('MAIL_HOST','XXXXX');
+define('MAIL_USERNAME','XXXXX');
+define('MAIL_PASSWORD','XXXXX');
+define('MAIL_SEND_FROM','XXXXX');
+define('MAIL_SEND_FROM_NAME','XXXXX');
+define('MAIL_REPLY_TO','XXXXX');
+define('MAIL_REPLY_TO_NAME','XXXXX');
+define('MAIL_PORT',XXXXX);
 
 define('APP_NAME', "Фабрика Меблів");
 define('APP_DESC', "Вебсайт для управління Фабрикою Меблів, ведення документації та звітів");
